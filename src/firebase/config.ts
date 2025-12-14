0ff663842722305cfb1f750e1386dd06e033ba85
Mad Apps Creator <madappscreator@gmail.com>
2025-12-14 17:41:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8928688313-be767",
+  "appId": "1:338422095586:web:5aad7ee746f69b0e87c8bb",
+  "apiKey": "AIzaSyCB3-bAipwrG-DQJDcf6PWk0lsIic9DIkc",
+  "authDomain": "studio-8928688313-be767.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "338422095586"
+};
