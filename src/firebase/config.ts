7f6dd9b40a871032a3f45bdbc6cf67c8fa3d7c45
Mad Apps Creator <madappscreator@gmail.com>
2025-12-15 15:16:33 +0000

    starting build "8c21405d-6065-478e-80cb-344b007d87a4" FETCHSOURCE BUILD

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyCB3-bAipwrG-DQJDcf6PWk0lsIic9DIkc",
-  authDomain: "studio-8928688313-be767.firebaseapp.com",
-  projectId: "studio-8928688313-be767",
-  storageBucket: "studio-8928688313-be767.appspot.com",
-  messagingSenderId: "338422095586",
-  appId: "1:338422095586:web:5aad7ee746f69b0e87c8bb"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
