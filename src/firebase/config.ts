9fd19d391e50f195b516046a5cd1f894ac52a094
Mad Apps Creator <madappscreator@gmail.com>
2025-12-15 12:35:38 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:338422095586:web:5aad7ee746f69b0e87c8bb",
   "apiKey": "AIzaSyCB3-bAipwrG-DQJDcf6PWk0lsIic9DIkc",
   "authDomain": "studio-8928688313-be767.firebaseapp.com",
+  "storageBucket": "studio-8928688313-be767.appspot.com",
   "measurementId": "",
   "messagingSenderId": "338422095586"
 };
