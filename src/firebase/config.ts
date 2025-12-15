78462bdb07d64f793c22f23d40dee28f981a1481
Mad Apps Creator <madappscreator@gmail.com>
2025-12-15 15:00:29 +0000

    Update the environment variables for firebase, I have mistakenly have tw

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8928688313-be767",
-  "appId": "1:338422095586:web:5aad7ee746f69b0e87c8bb",
-  "apiKey": "AIzaSyCB3-bAipwrG-DQJDcf6PWk0lsIic9DIkc",
-  "authDomain": "studio-8928688313-be767.firebaseapp.com",
-  "storageBucket": "studio-8928688313-be767.appspot.com",
-  "measurementId": "",
-  "messagingSenderId": "338422095586"
+  apiKey: "AIzaSyCB3-bAipwrG-DQJDcf6PWk0lsIic9DIkc",
+  authDomain: "studio-8928688313-be767.firebaseapp.com",
+  projectId: "studio-8928688313-be767",
+  storageBucket: "studio-8928688313-be767.appspot.com",
+  messagingSenderId: "338422095586",
+  appId: "1:338422095586:web:5aad7ee746f69b0e87c8bb"
 };
